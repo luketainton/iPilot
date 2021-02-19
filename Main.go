9d03c6f56717cddd0782551d5547c884ee5ae605
Luke Tainton <luke@tainton.uk>
2021-02-19 23:24:10 +0000

    Version 1.3: Allow disabling header

--- Main.go
+++ Main.go
@@ -7,14 +7,27 @@ import (
 )
 
 func main() {
-	printHeader()
-
 	var input string
 	var wantPrefixes bool
+	var wantHeader bool
 
-	flag.StringVar(&input, "i", "", "Specify IP address or domain name.")
-	flag.BoolVar(&wantPrefixes, "p", false, "Enable printing of advertised BGP prefixes.")
+	flag.StringVar(&input, "i", "", "IP address or domain")
+	flag.BoolVar(&wantPrefixes, "p", false, "print BGP prefixes")
+	flag.BoolVar(&wantHeader, "b", true, "enable/disable header")
+
+	flag.Usage = func() {
+		fmt.Printf("Usage of iPilot: \n")
+		fmt.Printf("  -b	bool	enable/disable header (default true)\n")
+		fmt.Printf("  -h	bool	view help\n")
+		fmt.Printf("  -i	string	IP address or domain\n")
+		fmt.Printf("  -p	bool	print BGP prefixes (default false)\n")
+	}
+
 	flag.Parse()
+
+	if wantHeader {
+		printHeader()
+	}
 
 	if input == "" {
 		fmt.Println("FATAL: No IP address or domain name was specified.")
